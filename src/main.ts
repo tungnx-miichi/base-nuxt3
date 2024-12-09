eb0bbb591862e16310504ed9ba8792757959e55f
tungnx <tungnx@miichisoft.com>
2024-12-09 10:56:20 +0700

    init base vue + vuetify + tailwind

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
+import '@mdi/font/css/materialdesignicons.css';
+import './styles/style.css';
+import router from './router';
+
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app.vue',
-    './components/**/*.{vue,js}',
-    './pages/**/*.{vue,js}',
-    './layouts/**/*.{vue,js}',
-    './composables/**/*.{js,ts}',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: '#f63b82', // Thêm sắc thái 'light'
-          DEFAULT: '#1E40AF', // Màu mặc định
-          dark: '#1E3A8A', // Thêm sắc thái 'dark'
-        },
-        secondary: '#9333EA',
-        red: '#e10909db'
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};

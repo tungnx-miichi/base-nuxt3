dfcd6e3959a9135a3aaaee00efb56dfd2fd7605b
Xuan Tung <admin@admins-MacBook-Pro.local>
2024-12-15 20:37:42 +0700

    map

--- src/map/index.ts
+++ src/map/index.ts
@@ -10,10 +10,46 @@ import { GEO_JSON_FUKUSHIMA } from './fukushima'
 import { GEO_JSON_GIFU } from './gifu'
 import { GEO_JSON_GUNMA } from './gunma'
 import { GEO_JSON_HIROSHIMA } from './hiroshima'
+import { GEO_JSON_HOKKAIDO } from './hokkaido'
+import { GEO_JSON_HYOGO } from './hyogo'
+import { GEO_JSON_IBARAKI } from './ibaraki'
+import { GEO_JSON_ISHIKAWA } from './ishikawa'
+import { GEO_JSON_IWATE } from './iwate'
+import { GEO_JSON_KAGAWA } from './kagawa'
+import { GEO_JSON_KAGOSHIMA } from './kagoshima'
+import { GEO_JSON_KANAGAWA } from './kanagawa'
+import { GEO_JSON_KOCHI } from './kochi'
+import { GEO_JSON_KUMAMOTO } from './kumamoto'
+import { GEO_JSON_KYOTO } from './kyoto'
+import { GEO_JSON_MIE } from './mie'
+import { GEO_JSON_MIYAGI } from './miyagi'
+import { GEO_JSON_MIYAZAKI } from './miyazaki'
+import { GEO_JSON_NAGANO } from './nagano'
+import { GEO_JSON_NAGASAKI } from './nagasaki'
+import { GEO_JSON_NARA } from './nara'
+import { GEO_JSON_NIIGATA } from './niigata'
+import { GEO_JSON_OITA } from './oita'
+import { GEO_JSON_OKAMA } from './okama'
+import { GEO_JSON_OKINAWA } from './okinawa'
+import { GEO_JSON_OSAKA } from './osaka'
+import { GEO_JSON_SAGA } from './saga'
+import { GEO_JSON_SAITAMA } from './saitama'
+import { GEO_JSON_SHIGA } from './shiga'
+import { GEO_JSON_SHIMANE } from './shimane'
+import { GEO_JSON_SHIZUOKA } from './shizuoka'
+import { GEO_JSON_TOCHIGI } from './tochigi'
+import { GEO_JSON_TOKUSHIMA } from './tokushima'
+import { GEO_JSON_TOKYO } from './tokyo'
+import { GEO_JSON_TOTTORI } from './tottori'
+import { GEO_JSON_TOYAMA } from './toyama'
+import { GEO_JSON_WAKAYAMA } from './wakayama'
+import { GEO_JSON_YAMAGATA } from './yamagata'
+import { GEO_JSON_YAMAGUCHI } from './yamaguchi'
+import { GEO_JSON_YAMANASHI } from './yamanashi'
 
 
 export const GEO_JSON_JAPAN = [
-    ...GEO_JSON_AICHI,
+    // ...GEO_JSON_AICHI,
     // ...GEO_JSON_AKITA,
     // ...GEO_JSON_AOMORI,
     // ...GEO_JSON_CHIBA,
@@ -24,4 +60,40 @@ export const GEO_JSON_JAPAN = [
     // ...GEO_JSON_GIFU,
     // ...GEO_JSON_GUNMA,
     // ...GEO_JSON_HIROSHIMA,
+    // ...GEO_JSON_HOKKAIDO,
+    // ...GEO_JSON_HYOGO,
+    // ...GEO_JSON_IBARAKI,
+    // ...GEO_JSON_ISHIKAWA,
+    // ...GEO_JSON_IWATE,
+    // ...GEO_JSON_KAGAWA,
+    // ...GEO_JSON_KAGOSHIMA,
+    // ...GEO_JSON_KANAGAWA,
+    // ...GEO_JSON_KOCHI,
+    // ...GEO_JSON_KUMAMOTO,
+    // ...GEO_JSON_KYOTO,
+    // ...GEO_JSON_MIE,
+    // ...GEO_JSON_MIYAGI,
+    // ...GEO_JSON_MIYAZAKI,
+    // ...GEO_JSON_NAGANO,
+    // ...GEO_JSON_NAGASAKI,
+    // ...GEO_JSON_NARA,
+    // ...GEO_JSON_NIIGATA,
+    // ...GEO_JSON_OITA,
+    // ...GEO_JSON_OKAMA,
+    // ...GEO_JSON_OKINAWA,
+    // ...GEO_JSON_OSAKA,
+    // ...GEO_JSON_SAGA,
+    // ...GEO_JSON_SAITAMA,
+    // ...GEO_JSON_SHIGA,
+    // ...GEO_JSON_SHIMANE,
+    // ...GEO_JSON_SHIZUOKA,
+    // ...GEO_JSON_TOCHIGI,
+    // ...GEO_JSON_TOKUSHIMA,
+    // ...GEO_JSON_TOKYO,
+    // ...GEO_JSON_TOTTORI,
+    // ...GEO_JSON_TOYAMA,
+    // ...GEO_JSON_WAKAYAMA,
+    // ...GEO_JSON_YAMAGATA,
+    // ...GEO_JSON_YAMAGUCHI,
+    ...GEO_JSON_YAMANASHI,
 ] 
